f6ceac84c386405d04f21ff0fb48919d0b2db921
Derrick <dbozich1@gmail.com>
2019-11-02 16:23:37 -0600

    prep for deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,3 +47,18 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /p5/,
+            use: loaders.null()
+          }
+        ]
+      }
+    })
+  }
+}
